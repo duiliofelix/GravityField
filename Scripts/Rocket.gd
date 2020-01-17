91c1ad30517124db61e8c1341a9f273f817c7d38
Duilio Felix <duiliofelix@gmail.com>
2020-01-17 08:07:54 -0300

    Adding side thrusters

--- Scripts/Rocket.gd
+++ Scripts/Rocket.gd
@@ -1,9 +1,14 @@
 extends RigidBody2D
 
 func _ready():
-	self.add_central_force(Vector2(0, -30))
+	pass
 	
 func _input(event):
 	if Input.is_key_pressed(KEY_SPACE):
-		self.add_central_force(Vector2(0, -30))
+		self.add_central_force($BaseThrust.cast_to)
 		
+	if Input.is_key_pressed(KEY_RIGHT):
+		self.add_force($RightThruster.transform.get_origin(), $RightThruster.cast_to)
+		
+	if Input.is_key_pressed(KEY_LEFT):
+		self.add_force($LeftThruster.transform.get_origin(), $LeftThruster.cast_to)
