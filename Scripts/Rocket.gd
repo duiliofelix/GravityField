11a0767d836b18c874e8415e6a5809ad2a4e1f50
Duilio Felix <duilio.felix@softwrap.com.br>
2020-01-17 20:06:25 -0300

    Adding directional thrust

--- Scripts/Rocket.gd
+++ Scripts/Rocket.gd
@@ -2,10 +2,12 @@ extends RigidBody2D
 
 func _ready():
 	pass
-	
+
 func _input(event):
 	if Input.is_key_pressed(KEY_SPACE):
-		self.add_central_force($BaseThrust.cast_to)
+		var thrust = $BaseThrust.cast_to
+		var directed_thrust = thrust.rotated(self.transform.get_rotation())
+		self.add_central_force(directed_thrust)
 		
 	if Input.is_key_pressed(KEY_RIGHT):
 		self.add_force($RightThruster.transform.get_origin(), $RightThruster.cast_to)
